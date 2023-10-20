60164a142b7e8e324cdd3ec60fc40387afda4e88
LucasRocha <lucascaixeta98@gmail.com>
2023-10-20 15:27:15 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/protxprts/demo/config/TestConfig.java
+++ src/main/java/com/protxprts/demo/config/TestConfig.java
@@ -41,7 +41,8 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 
-		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, " + "consectetur.", 90.5, "");
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, " + "consectetur.", 90.5,
+				"");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. " + "Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
@@ -50,14 +51,20 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+		// acosiacao de objetos
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 		User u1 = new User(null, "Fernanda Oliveira", "fernanda@gmail.com", "988888888", "123456");
-
 		User u2 = new User(null, "Lucas Green", "greem@gmail.com", "977777777", "123456");
-
 		Order o1 = new Order(null, Instant.parse("2023-06-20T19:53:07Z"), OrderStatus.PAID, u1);
-
 		Order o2 = new Order(null, Instant.parse("2023-07-21T03:42:10Z"), OrderStatus.WAITIGN_PAYMENT, u2);
-
 		Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), OrderStatus.WAITIGN_PAYMENT, u1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));

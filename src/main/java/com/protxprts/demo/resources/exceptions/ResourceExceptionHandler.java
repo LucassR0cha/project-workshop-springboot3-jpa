8939cf57910cc622030c0efc99e5819e6679344b
LucasRocha <lucascaixeta98@gmail.com>
2023-10-22 16:34:33 -0300

    Exception handling - To Delete

--- src/main/java/com/protxprts/demo/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/protxprts/demo/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.protxprts.demo.services.exceptions.DatabaseException;
 import com.protxprts.demo.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,6 +20,17 @@ public class ResourceExceptionHandler {
 
 		String error = "Recurso não encontrado";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError str = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+
+		return ResponseEntity.status(status).body(str);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+
+		String error = "Erro no Banco de Dados";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError str = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
 				request.getRequestURI());
 
